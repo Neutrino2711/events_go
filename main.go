8a443f3a0a4fddab3e16ac9f6d409eb3db3f5557
Akshaj77 <cse21008@gmail.com>
2024-07-12 21:56:57 +0530

    initial-get-post-requests

--- main.go
+++ main.go
@@ -1,5 +1,41 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+
+	"example.com/events/models"
+	"github.com/gin-gonic/gin"
+)
+
 func main() {
+	server := gin.Default()
+
+	server.GET("/events", getEvent)
+	server.POST("/events", createEvent)
+
+	server.Run(":8080")
+
+}
+
+func getEvent(context *gin.Context) {
+	events := models.GetAllEvents()
+	context.JSON(http.StatusOK, events)
+}
+
+func createEvent(context *gin.Context) {
+	var event models.Event
+	err := context.ShouldBindJSON(&event)
+	fmt.Println(event)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse data."})
+		return
+	}
+
+	event.ID = 1
+	event.UserID = 1
+
+	event.Save()
+	context.JSON(http.StatusCreated, gin.H{"message": "Event Created!", "event": event})
 
 }
