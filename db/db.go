4c9a5457d2ce7fbbb62078f5e262966c5492180d
Akshaj77 <cse21008@gmail.com>
2024-07-14 19:57:59 +0530

    create-table-db

--- db/db.go
+++ db/db.go
@@ -9,7 +9,8 @@ import (
 var DB *sql.DB
 
 func InitDB() {
-	DB, err := sql.Open("sqlite3", "api.db")
+	var err error
+	DB, err = sql.Open("sqlite3", "api.db")
 
 	if err != nil {
 		panic("could not connect to database")
@@ -17,5 +18,27 @@ func InitDB() {
 
 	DB.SetMaxIdleConns(5)
 	DB.SetMaxOpenConns(10)
+
+	createTables()
+}
+
+func createTables() {
+
+	createEventsTable := `	
+	CREATE TABLE IF NOT EXISTS events (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	name TEXT NOT NULL,
+	description TEXT NOT NULL,
+	location TEXT NOT NULL,
+	dateTime DATETIME NOT NULL,
+	user_id INTEGER 
+	)
+	`
+
+	_, err := DB.Exec(createEventsTable)
+
+	if err != nil {
+		panic("Could not create events table.")
+	}
 
 }
