023985ccb71fffcb109858ac3477e20002e58a06
Akshaj77 <cse21008@gmail.com>
2024-07-19 17:27:30 +0530

    jwt-token-added

--- routes/users.go
+++ routes/users.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"example.com/events/models"
+	"example.com/events/utils"
 	"github.com/gin-gonic/gin"
 )
 
@@ -44,6 +45,12 @@ func login(context *gin.Context) {
 		return
 	}
 
-	context.JSON(http.StatusOK, gin.H{"message": "Login successful!"})
+	token, err := utils.GenerateToken(user.Email, user.ID)
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "could not authenticate user."})
+	}
+
+	context.JSON(http.StatusOK, gin.H{"message": "Login successful!", "token": token})
 
 }
